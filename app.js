46a5c33639cd2cb38c17f40fc9e3864338d7a1ed
Erin Carter <me@erincarter.com>
2012-10-20 20:33:21 -0500

    basic socket connection

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , sio = require('socket.io');
 
 var app = express();
 
@@ -30,6 +31,14 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+
+app.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = sio.listen(server);
+
+io.sockets.on('connection', function(socket){
+  console.log('socket',socket);
 });
